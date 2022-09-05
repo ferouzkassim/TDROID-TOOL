d9163916ac96d9b27bbf27f14976d5f515712504
ferouz kassim <ferouzkassim@gmail.com>
2022-09-05 05:18:15 +0300

    added adb
    adding adb

--- gui.py
+++ gui.py
@@ -17,7 +17,7 @@ cent_screenh = int(scree_height/2-root_hi)
 root.geometry(f'{root_wid}x{root_hi}+{cent_screen}+{cent_screenh}')
 #end of root sizings
 #labells
-lebo = Label(root,text='model',padx=30,pady=10).place(x=400,y=0)
+#lebo = Label(root,text='model',padx=30,pady=10).place(x=400,y=0)
 #frames
 frem = Frame(root, height=600, width=100, )
 frem.columnconfigure(0,weight=1)
@@ -28,21 +28,17 @@ frem2.grid(column=1,row=1)
 #button
 def butt(txt, nem ):
     nem = tk.Button(frem, text=txt, )
-    nem.configure(bg='black', foreground='white', borderwidth=2,width=14)
+    nem.configure(bg='black', foreground='black', borderwidth=2,width=14)
     nem.grid(pady=10,padx=10,sticky=W)
     return nem
 
 #detect=tk.Button(frem,text='Detect',)
 #model list
-mdellist=['sm-a125f','sm-a127f','sm-a135f','sm-a022f','sm-a225f']
-for umdel in mdellist:
-    umdel.upper()
 
-#drop list
-model=tk.Listbox(root,height=2,listvariable=tk.StringVar(value=(for umdel in mdellist:
-umdel.upper())))
+'''drop list
+model= tk.Listbox(root,height=5,listvariable=tk.StringVar(value=mdellist))
 model.grid(row=0,column=10,sticky=NW,pady=25,padx=10)
-
+'''
 
 
 #detect.place(x=10,y=50)
@@ -54,16 +50,32 @@ Detect.grid(row=0,column=0,)
 Restore=butt(txt='Restore Nv',nem='Restore')
 Restore.grid(row=2,column=0)
 fix=butt(txt='fix baseband',nem='fix')
-fix.configure(font='arial 10')
+fix.configure(font='arial 10',width=14)
 fix.grid(row=3,column=0)
 mount=butt(txt='mount baseband',nem='mount')
 mount.grid(row=4,column=0)
+mount.configure(width=14)
 mount.config(font='ubuntu 9')
 mount.config(padx=10)
 BackUpEfs=butt(txt='Backup Efs',nem='BackupEfs')
 BackUpEfs.grid(row=5,column=0)
 
 
-#textare
+def model_selection():
+    def selection(model):
+        print(model)
+        '''if model == str('sm-a125f'):
+            print('the selectd model is mtk')
+'''
+        if model == str('sm-a127f') or model == str('sm-a135f'):
+          print('selected model is eynos')
+    mdellist = ['sm-a125f', 'sm-a127f', 'sm-a135f', 'sm-a022f', 'sm-a225f']
+    fast = StringVar()
+    fast.set('model')
+    modeloption = OptionMenu(root, fast, *mdellist, command=selection)
+    modeloption.place(x=400, y=10)
 
+
+
+model_selection()
 root.mainloop()
