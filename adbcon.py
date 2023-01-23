2f63ac874fc19fe9dae24679f782f0d57c657bb3
ferouz kassim <ferouzkassim@gmail.com>
2023-01-23 13:10:47 +0300

    changing gui to add flasher interface

--- adbcon.py
+++ adbcon.py
@@ -2,29 +2,33 @@ import subprocess
 import tkinter
 import time
 from ppadb.client import Client as AdbClient
-    # Default is "127.0.0.1" and 5037
+
+# Default is "127.0.0.1" and 5037
 logs = open('logs/logs.txt', 'w')
-host='127.0.0.1'
-port=5037
+host = '127.0.0.1'
+port = 5037
+
 
 class startDaemon:
     # try:
     start = subprocess.run(['daemon/adb.exe', 'start-server'])
-client = AdbClient(host,port)
+
+
+client = AdbClient(host, port)
 for device in client.devices():
     snmbr = device.serial
+
+
 # starting
 #
 # the adb server using adb.exe
 def startShell():
     startDaemon.start
-    #client.create_connection(timeout=200)
+    # client.create_connection(timeout=200)
     client.devices()
-    for device in client.devices():
-        print(device.serial)
-    '''for package in device.list_packages():
-            package = logs'''
+    for initdevice in client.devices():
+        print(initdevice.serial)
 
-
-
-
+    for package in device.list_packages():
+            print(package)
+startShell()
--- usbcom.py
+++ usbcom.py
@@ -1,5 +1,6 @@
 import usb
-
+from usb import *
 for dev in usb.core.find(find_all=True):
-    pass
-
+    prod = dev.port_number
+    conf=dev.configurations()
+usb.core.show_devices(True)
